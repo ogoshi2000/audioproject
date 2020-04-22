2ecfc6cce68ee975fd805b0b8cda599982a1bb8a
Raphael Schnitzler <wattx@wattxs-mbp-2.speedport.ip>
2020-04-22 18:59:35 +0200

    sleeper added bug fixed

--- audio.py
+++ audio.py
@@ -2,9 +2,10 @@ import pyaudio
 import numpy as np
 from scipy import signal
 import struct
+import time
 
 
-chunk = 1024  # Record in chunks of 1024 samples
+chunk = 2048  # Record in chunks of 1024 samples
 sample_format = pyaudio.paInt16  # 16 bits per sample
 channels = 2
 fs = 44100  # Record at 44100 samples per second
@@ -24,8 +25,12 @@ frames = []  # Initialize array to store frames
 # Store data in chunks for 3 seconds
 while True:
     data = stream.read(chunk)
-    data = np.array(struct.unpack(str(2 * chunk) + 'B', data), dtype='b')
+
+    data = np.frombuffer(data, dtype='b')
     f, t, Sxx = signal.spectrogram(data, fs=chunk)
     dBS = 10 * np.log10(Sxx)
+
+    print(f)
+    time.sleep(1/15000)
 
     # TODO: translate to duty cycles, also do the fft correctly and this other stuff we were talking about
