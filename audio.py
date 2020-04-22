b87407048ac744bcded92bb7542d48d5ef8490ad
Art Joshua Robert Pelling <art.jr.pelling@campus.tu-berlin.de>
2020-04-22 21:39:18 +0200

    improve plotting

--- audio.py
+++ audio.py
@@ -3,6 +3,11 @@ import numpy as np
 import time
 import matplotlib.pyplot as plt
 
+
+def dBFS(x):
+    return 10*np.log10(x)
+
+
 chunk = 2048  # Record in chunks of 1024 samples
 sample_format = pyaudio.paInt16  # 16 bits per sample
 channels = 1
@@ -29,8 +34,8 @@ fig = plt.figure(1)
 ax1 = fig.add_subplot(211)
 ax2 = fig.add_subplot(212)
 fig.show()
-mx1 = 0
-mx2 = 0
+mx1 = 20000
+mx2 = 80
 
 while True:
     data = stream.read(chunk)
@@ -38,26 +43,23 @@ while True:
     fourier_data = np.fft.rfft(data)
 
     for i, idx in enumerate(fidx):
-        bands[i] = np.sqrt(np.mean(abs(fourier_data[idx])**2, axis=-1))
+        bands[i] = dBFS(np.sqrt(np.mean(abs(fourier_data[idx])**2, axis=-1)))
 
     ax1.clear()
     ax1.plot(data)
-    if np.max(np.abs(data)) > mx1:
-        mx1 = np.max(np.abs(data))
-        ax1.set_ylim([-mx1, mx1])
+    ax1.set_ylim([-mx1, mx1])
     ax1.grid(b=True)
 
     ax2.clear()
     ax2.bar(np.arange(bands.shape[0]),
             bands,
             tick_label=[str(cf) for cf in oc_bands])
-    if np.max(bands) > mx2:
-        mx2 = np.max(bands)
-        ax2.set_ylim([0, mx2])
+
+    ax2.set_ylim([0, mx2])
     ax2.grid(b=True)
     fig.canvas.draw()
 
-    plt.pause(0.001)
+    plt.pause(0.005)
     # TODO: translate to duty cycles
 
 p.close(stream)
