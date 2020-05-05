72e2c04efadce5347a4ffb67851a4abe5d5e1421
Art Joshua Robert Pelling <art.jr.pelling@campus.tu-berlin.de>
2020-05-05 14:25:51 +0200

    commit

--- audio.py
+++ audio.py
@@ -1,6 +1,7 @@
 import pyaudio
 import numpy as np
 import time
+import queue
 
 
 def dBFS(x):
@@ -24,35 +25,47 @@ for cf in OC_BANDS:
 bands = np.zeros(OC_BANDS.shape)
 
 p = pyaudio.PyAudio()
+
+frames = queue.Queue()
+
+def callback(in_data, frame_count, time_info, status):
+    global frames
+    frames.put(np.frombuffer(in_data, dtype=np.int16))
+    return (in_data, pyaudio.paContinue)
+
 stream = p.open(format=SAMPLE_FORMAT,
                 channels=CHANNELS,
                 rate=FS,
                 frames_per_buffer=CHUNK,
-                input=True)
+                input=True,
+                stream_callback=callback)
 
 OLIDX = [i * CHUNK / OVERLAPS for i in range(OVERLAPS)]
 print(OLIDX)
 packets = 0
 lost = 0
-prev_chunk = stream.read(CHUNK, exception_on_overflow=True)
-prev_chunk = np.frombuffer(prev_chunk, dtype=np.int16)
+stream.start_stream()
 while True:
-    try:
-        packets += 1
-        curr_chunk = stream.read(CHUNK, exception_on_overflow=True)
-    except IOError:
-        lost += 1
-        print(str(lost) + '/' + str(packets))
+    print(frames.get())
+# prev_chunk = stream.read(CHUNK, exception_on_overflow=True)
+# prev_chunk = np.frombuffer(prev_chunk, dtype=np.int16)
+# while True:
+#     try:
+#         packets += 1
+#         curr_chunk = stream.read(CHUNK, exception_on_overflow=True)
+#     except IOError:
+#         lost += 1
+#         print(str(lost) + '/' + str(packets))
 
-    curr_chunk = np.frombuffer(curr_chunk, dtype=np.int16)
+#     curr_chunk = np.frombuffer(curr_chunk, dtype=np.int16)
 
-    for k in OLIDX:
-        fourier_data = np.fft.rfft(
-            np.concatenate([prev_chunk[:k], curr_chunk[k:]]))
-        for i, idx in enumerate(fidx):
-            bands[i] = dBFS(np.sqrt(np.sum(abs(fourier_data[idx])**2,
-                                           axis=-1)))
+#     for k in OLIDX:
+#         fourier_data = np.fft.rfft(
+#             np.concatenate([prev_chunk[:k], curr_chunk[k:]]))
+#         for i, idx in enumerate(fidx):
+#             bands[i] = dBFS(np.sqrt(np.sum(abs(fourier_data[idx])**2,
+#                                            axis=-1)))
 
-    prev_chunk = curr_chunk
+#     prev_chunk = curr_chunk
 
 p.close(stream)
