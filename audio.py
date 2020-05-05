33358fcba3a959ed8dd59731a8b92a54e8114946
Art Joshua Robert Pelling <art.jr.pelling@campus.tu-berlin.de>
2020-05-05 21:42:02 +0200

    commit

--- audio.py
+++ audio.py
@@ -10,8 +10,9 @@ def dBFS(x):
 
 
 ###############################################################################
-CHUNK = 64  #
-OVERLAPS = 2  #
+FFTCHUNK = 256
+CHUNK = 128  #
+OVERLAPS = 4  #
 SAMPLE_FORMAT = pyaudio.paInt16  #
 CHANNELS = 1  #
 FS = 48000  #
@@ -19,7 +20,7 @@ OC_BANDS = np.array([31.5, 63, 125, 250, 500, 1000, 2000, 4000, 8000,
                      16000])  #
 ###############################################################################
 
-F = np.fft.rfftfreq(CHUNK * OVERLAPS, 1. / FS)
+F = np.fft.rfftfreq(FFTCHUNK, 1. / FS)
 fidx = []
 for cf in OC_BANDS:
     fidx.append(np.where((F >= cf / np.sqrt(2)) & (F <= cf * np.sqrt(2))))
@@ -45,18 +46,21 @@ stream = p.open(format=SAMPLE_FORMAT,
                 stream_callback=callback)
 
 curr_chunk = np.zeros(CHUNK * OVERLAPS)
-OLIDX = [i * CHUNK / OVERLAPS for i in range(OVERLAPS)]
+OL_IDX = [i * CHUNK / OVERLAPS for i in range(OVERLAPS * CHUNK / FFTCHUNK + 1)]
 start = time.time()
 try:
     while True:
-        curr_chunk = np.concatenate([curr_chunk[CHUNK:], frames.get()])
-        fourier_data = scipy.fftpack.fft(curr_chunk)
-        for i, idx in enumerate(fidx):
-            bands[i] = dBFS(np.sqrt(np.sum(abs(fourier_data[idx])**2,
-                                           axis=-1)))
-        if int(time.time() - start) == 30:
-            start = time.time()
-            print(frames.qsize() * float(CHUNK) / FS)
+        frame = frames.get()
+        for i, j in zip(OL_IDX, OL_IDX[1:]):
+            curr_chunk = np.concatenate(
+                [curr_chunk[FFTCHUNK / OVERLAPS:], frame[i:j]])
+            for k, idx in enumerate(fidx):
+                fourier_data = scipy.fftpack.fft(curr_chunk)
+                bands[k] = dBFS(
+                    np.sqrt(np.sum(abs(fourier_data[idx])**2, axis=-1)))
+            if int(time.time() - start) == 30:
+                start = time.time()
+                print(frames.qsize() * float(CHUNK) / FS)
 except KeyboardInterrupt:
     stream.stop_stream()
     stream.close()
