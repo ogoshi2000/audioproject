c7961e976d384d94e6abcf1a8b1d446ba74efd99
Art Joshua Robert Pelling <art.jr.pelling@campus.tu-berlin.de>
2020-05-06 14:15:32 +0200

    commit

--- audio.py
+++ audio.py
@@ -12,12 +12,12 @@ def dBFS(x):
 
 
 ###############################################################################
-FFTCHUNK = 1024
-CHUNK = 128  #
+FFTCHUNK = 256
+CHUNK = 64  #
 SAMPLE_FORMAT = pyaudio.paInt16  #
 CHANNELS = 1  #
 FS = 48000  #
-OC_BANDS = np.array([0, 63, 125, 250, 500, 1000, 2000, 4000, 8000, 16000])  #
+OC_BANDS = np.array([125, 250, 500, 1000, 2000, 4000, 8000, 16000])  #
 ###############################################################################
 F = np.fft.rfftfreq(FFTCHUNK, 1. / FS)
 fidx = []
@@ -50,7 +50,7 @@ try:
     while True:
         curr_chunk = np.concatenate([curr_chunk[CHUNK:], frames.get()])
         for k, idx in enumerate(fidx):
-            fourier_data = np.fft.rfft(curr_chunk)
+            fourier_data = scipy.fftpack.fft(curr_chunk)
             bands[k] = dBFS(np.sqrt(np.sum(abs(fourier_data[idx])**2,
                                            axis=-1)))
 
