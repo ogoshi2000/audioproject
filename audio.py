e6ce3a203207ec4bcf726fb2aee78fd7df5b6665
Raphael Schnitzler <wattx@wattxs-MBP-2.fritz.box>
2020-05-20 19:02:59 +0200

    nomessage

--- audio.py
+++ audio.py
@@ -8,11 +8,10 @@ import sys
 i2c = busio.I2C(board.SCL, board.SDA)
 hat = adafruit_pca9685.PCA9685(i2c)
 
-hat.frequency = int(sys.argv[0])
+hat.frequency = int(sys.argv[1])
 led_channel = hat.channels[0]
 
-led_channel.duty_cycle = int(sys.argv[1]**2)/2
+led_channel.duty_cycle = int(sys.argv[2]**2)/2
 
 while True:
     pass
-
