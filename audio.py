ab640e714a8984a8b09b629a117494562786a3cc
Raphael Schnitzler <wattx@wattxs-MBP-2.fritz.box>
2020-05-20 19:12:33 +0200

    blablabla

--- audio.py
+++ audio.py
@@ -10,8 +10,9 @@ hat = adafruit_pca9685.PCA9685(i2c)
 
 hat.frequency = int(sys.argv[1])
 led_channel = hat.channels[0]
+cycle = int(sys.argv[2])**2
 
-led_channel.duty_cycle = (int(sys.argv[2])**2)/2
+led_channel.duty_cycle = cycle/2
 
 while True:
     pass
